9718e35389f5ce314eac3a7fb6ec32045828da24
Ivanna <up240223@alumnos.upa.edu.mx>
2026-03-02 20:14:19 -0600

    add fetcha getPokemons

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,30 @@
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  const[results, setresurts] = useState<any>()
+  useEffect(() => {
+    console.log("Entre entre en pantalla")
+    getPokemons();
+  }, []);
+
+  //Funcion flecha
+  const getPokemons = async () => {
+    const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+    const response = await fetch(URL, {
+      method: "GET",
+    });
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+  };
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text>Ivanna</Text>
     </View>
   );
 }
+
+
+//API
+//Es una una manera de conectar de forma local o remota con un API
